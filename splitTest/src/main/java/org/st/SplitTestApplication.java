ebd9795c7dcc97897478f8fbb091337d0cc98e4f
nodoka13 <nodoka13@gmail.com>
2024-11-24 14:24:01 +0900

    Spring Boot Set

---
+++ splitTest/src/main/java/org/st/SplitTestApplication.java
@@ -0,0 +1,13 @@
+package org.st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitTestApplication.class, args);
+	}
+
+}
